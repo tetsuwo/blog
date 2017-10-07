2000675cea0a2c21ff819f61339427eac678f60c
tetsuwo <tetsuwo.oishi@gmail.com>
2017-10-07 11:38:03 +0900

    シーケンス図まわり調整

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,1 +1,16 @@
-console.log('hoge');
+document.addEventListener('DOMContentLoaded', function(event) {
+    var elements = document.querySelectorAll('.language-sequence');
+
+    _.forEach(elements, function(el) {
+        var diagramText = el.innerText;
+        console.log(el.tagName, diagramText);
+        var diagram = Diagram.parse(diagramText);
+
+        var div = document.createElement('div');
+
+        var parentNode = el.parentElement.parentNode;
+        parentNode.replaceChild(div, el.parentElement);
+
+        diagram.drawSVG(div, { theme: 'simple' });
+    });
+});
---
+++ tag/API Framework.md
@@ -0,0 +1,5 @@
+---
+layout: posts-tag
+title: "Tag: API Framework"
+tag: API Framework
+---
